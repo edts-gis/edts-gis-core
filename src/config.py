730987a93a44df7611e72541e57bf2180c9b9830
leonardif-edts <leonardi.fabianto@sg-edts.com>
2024-10-17 03:13:54 +0700

    deploy: fix cloudbuild with cloud sql

--- src/config.py
+++ src/config.py
@@ -12,16 +12,21 @@ class Config(BaseSettings):
     DB_PASSWORD: SecretStr
     DB_PORT: int
     DB_NAME: str
+    CLOUD_SQL_INSTANCE_UNIX_SOCKET: Optional[str] = None
 
     @computed_field
     @property
     def DB_URI(self) -> URL:
+        query = {
+            "unix_sock": f"{self.CLOUD_SQL_INSTANCE_UNIX_SOCKET}/.s.PGSQL.{self.DB_PORT}"
+        } if self.CLOUD_SQL_INSTANCE_UNIX_SOCKET else None
         db_uri = URL.create(
             drivername = self.DB_DRIVER,
             username = self.DB_USERNAME,
             password = self.DB_PASSWORD.get_secret_value(),
             host = self.DB_HOSTNAME,
             port = self.DB_PORT,
-            database = self.DB_NAME
+            database = self.DB_NAME,
+            query = query
         )
         return db_uri
